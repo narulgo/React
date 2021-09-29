f5f9b7c853664b7f3e389c1ac8c860cff643c266
Narul <naruln9494@gmail.com>
2021-09-29 14:22:13 +0600

    Arrow functions

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-function myName() {
+const obj = {
 
-    return this.myname
+    myFunction: function () {
+
+        return this
+
+    },
+
+    coolFunction: () => {
+
+        return this
+
+    }
 
 }
 
-const myNameButArrow = () => console.log(this.myname)
+console.log(obj.myFunction())
 
-const person1 = { myname: "Mehul", myName }
-
-const person2 = { myname: "James", myName }
-
-const person3 = { myname: "Enzo", myName: myNameButArrow }
-
-console.log(person1.myName())
-
-console.log(person2.myName())
-
-console.log(person3.myName())
+console.log(obj.coolFunction())
