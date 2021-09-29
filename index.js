f8c273d64a2db57bcf0ac43f3ccbd06074e7aec4
Narul <naruln9494@gmail.com>
2021-09-29 14:27:48 +0600

    Classes

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
-x = [1, 2, 3, 4, 5]
+class ILoveNumbers {
 
-x.reduce((acc, val) => acc + val, 0)
+    constructor(num) {
+
+        this.anothernum = num
+
+    }
+
+    getNum() {
+
+        console.log(this.anothernum)
+
+    }
+
+};
+
+class IHateNumbers extends ILoveNumbers {
+
+    constructor(num) {
+
+        super(num ** num)
+
+        this.num = num
+
+    }
+
+    getNum() {
+
+        console.log(this.num)
+
+    }
+
+    getNumFromParent() {
+
+        console.log(super.getNum())
+
+    }
+
+};
+
+let obj = new IHateNumbers(5)
+
+obj.getNum()
+
+obj.getNumFromParent()
